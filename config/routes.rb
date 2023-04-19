1c0026e30963bdec1eb25b8529fb94ba2c535df7
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-04-19 02:51:03 +0000

    [finish]user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'homes/about'
 
   resources :books, only: [:new, :create, :edit, :index, :show, :update, :destroy]
-  resources :users, only: [:edit, :index, :show]
+  resources :users, only: [:edit, :index, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
