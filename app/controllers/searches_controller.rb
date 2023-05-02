581f6adf7c9b7f2010252eaa9b70e661575a3596
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-05-02 04:48:32 +0000

    [finish]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,17 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-  end
+
 
   def search
     @range = params[:range]
+    @word = params[:word]
 
-    if @range == "User"
+    if @range == 'User'
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
     end
+    render "index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   get "search" => "searches#search"
-  resources :searches, only: [:index]
 end
 
