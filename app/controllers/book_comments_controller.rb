af0ec7691d69b4c67d4ad71777f7a55cc8609bbe
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-05-03 07:58:14 +0000

    [finish]応用課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,16 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    flash.now[:notice] = 'コメントを投稿しました'
+    render :book_comment
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    flash.now[:alert] = 'コメントを削除しました'
+    book = Book.find(params[:book_id])
+    render :book_comment
+
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
+  before_action :book_params
+
   def index
     @book = Book.all
   end
 
   def create
     Favorite.create(user_id: current_user.id, book_id: params[:id])
-    redirect_to books_path
   end
 
   def destroy
     Favorite.find_by(user_id: current_user.id, book_id: params[:id]).destroy
-    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    @book = Book.find(params[:id])
   end
 end
