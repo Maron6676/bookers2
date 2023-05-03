ec10fde5d242b024475fdfd26b16ec043e72aab1
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-05-03 00:37:45 +0000

    [start]応用課題６

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_30_043645) do
+ActiveRecord::Schema.define(version: 2023_05_03_003221) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2023_04_30_043645) do
     t.string "title"
     t.string "body"
     t.integer "user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "book_id"
   end
 
   create_table "relationships", force: :cascade do |t|
